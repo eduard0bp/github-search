44a5c17f474a18d95179d0b60daaeec36f6daae4
Eduardo Batista Pereira <eduardo.pereira@southsystem.com.br>
2022-11-17 21:07:16 -0300

    Initial commit

---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import Container from '../components/container'
+import Header from '../components/header'
+import UserContainer from '../components/userContainer'
+
+export default function Home() {
+  return (
+    <Container>
+      <Header />
+      <UserContainer></UserContainer>
+    </Container>
+  )
+}
